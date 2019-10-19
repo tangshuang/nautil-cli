558769b1861300c3fc950de5c0a0d8008232ea16
frustigor <frustigor@gmail.com>
2019-10-19 11:23:36 +0800

    update

--- configs/css-loader.config.js
+++ configs/css-loader.config.js
@@ -2,8 +2,7 @@ module.exports = {
   loader: 'css-loader',
   options: {
     modules: {
-      localIdentName: process.env.NODE_ENV === 'production' ? '[hash:base64]' : '[path]__[name]__[local]',
-      hashPrefix: 'hash',
+      localIdentName: process.env.NODE_ENV === 'production' ? '[hash:base64]' : '[path][name]__[local]',
     },
     localsConvention: 'camelCaseOnly',
   },
